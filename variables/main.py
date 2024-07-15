86998786d0bc1652911835cc637454a0d1522fc9
turboaux <jje.uribe@gmail.com>
2024-07-15 10:19:04 -0600

    Complete Homework #1: Variables

---
+++ variables/main.py
@@ -0,0 +1,22 @@
+# Song's name
+song = "Final Frontier"
+# Song's Genre
+genre = "Electronic"
+# Song's artist
+artist = "Juno Reactor"
+# Song's album
+album = "The Golden Sun of the Great East"
+# Song's duration in seconds
+duration_in_seconds = 420
+# Likes number
+number_of_likes = 1000000
+# Times downloaded
+times_downloaded = 500000
+
+print(song)
+print(genre)
+print(artist)
+print(album)
+print(duration_in_seconds)
+print(number_of_likes)
+print(times_downloaded)
