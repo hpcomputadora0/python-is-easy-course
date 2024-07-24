b484ac57f959ea826d43911dc2c88ec3d63fc015
turboaux <jje.uribe@gmail.com>
2024-07-24 14:04:02 -0600

    Complete Homework #2: Functions

---
+++ functions/main.py
@@ -0,0 +1,16 @@
+def get_song(): 
+	return 'Final Frontier'
+
+def get_artist(): 
+    return 'Juno Reactor'
+
+def get_album(): 
+    return 'The Golden Sun of the Great East'
+
+def is_a_good_song(): 
+    return True
+
+print(get_song())
+print(get_artist())
+print(get_album())
+print(is_a_good_song())
